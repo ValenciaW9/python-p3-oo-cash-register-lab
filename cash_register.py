3517f3fcd8dc472703fa4c35f266bd5eed8773b8
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-05 20:54:17 -0500

    python-p3-oo-cash-register-lab/
    python-p3-oo-cash-register-lab/

--- cash_register.py
+++ cash_register.py
@@ -5,15 +5,18 @@ class CashRegister:
         self.items = []
 
     def add_item(self, title, price, quantity=1):
-        self.total += price * quantity
+        self.total += float(price) * quantity
         self.items.extend([title] * quantity)
 
     def apply_discount(self):
         if self.discount > 0:
             discount_amount = self.total * (self.discount / 100)
             self.total -= discount_amount
+            print(f"After the discount, the total comes to ${self.total:.2f}.")
+        else:
+            print("There is no discount to apply.")
 
     def void_last_transaction(self):
         if self.items:
-            last_price = self.total - self.items.pop()
+            last_price = float(self.items.pop())
             self.total -= last_price
