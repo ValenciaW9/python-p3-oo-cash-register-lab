ed9aca989e6202c00d7bf67c0ab37d312a35e164
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-05 21:03:24 -0500

    python-p3-oo-cash-register-lab/
    python-p3-oo-cash-register-lab/

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -12,6 +12,7 @@ class CashRegister:
         if self.discount > 0:
             discount_amount = self.total * (self.discount / 100)
             self.total -= discount_amount
+            self.total = round(self.total, 2)
             print(f"After the discount, the total comes to ${self.total:.2f}.")
         else:
             print("There is no discount to apply.")
